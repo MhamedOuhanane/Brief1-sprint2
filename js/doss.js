4dcb5c78f49a6579178862da5d200cdaa5297c08
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-10-31 16:58:19 +0100

    Lier le dossier javascript avec le fichier HTML

---
+++ js/doss.js
@@ -0,0 +1,5 @@
+let task_title = document.querySelector("#title");
+let task_descr = document.querySelector("#descreption");
+let task_status = document.querySelector("#status");
+let task_date = document.querySelector("#date");
+let task_priority = document.querySelector("#priority");
