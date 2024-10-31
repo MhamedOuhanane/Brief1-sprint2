ce320731c198e9d5f14aebdb981cec8b3073cc28
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-10-31 17:30:29 +0100

    déclarer les variables utilisées dans le script JavaScript

--- js/doss.js
+++ js/doss.js
@@ -3,3 +3,14 @@ let task_descr = document.querySelector("#descreption");
 let task_status = document.querySelector("#status");
 let task_date = document.querySelector("#date");
 let task_priority = document.querySelector("#priority");
+let Add = document.querySelector(".submit");
+let div = document.createElement("div");
+div.className = "task"
+document.getElementsByClassName(".To Do");
+document.getElementsByClassName(".Doing");
+document.getElementsByClassName(".Done");
+
+Add.onclick = fonction() {
+    
+    
+}
