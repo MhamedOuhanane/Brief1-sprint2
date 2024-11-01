1829907ff66229be141f5344e12690cbdcdbd43a
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-01 17:32:28 +0100

    modifier script de card ajoute

--- js/doss.js
+++ js/doss.js
@@ -1,56 +1,93 @@
-let task_title = document.getElementById("title").value;
-task_title.className = "task-title";
-// let task_descr = document.getElementById("description").value;
-let task_status = document.getElementById("status").value;
-let task_time = document.getElementById("time").value;
-let task_date = document.getElementById("date").value;
-let task_priority = document.getElementById("priority").value;
+let task_title = document.getElementById("title");
+let card_title = document.createElement('div');
+card_title.className = "task-title";
 
+let task_descr = document.getElementById("description");
+let card_descr = document.createElement('div');
+card_descr.className = "task-descr";
 
-task_title.className = "task-title";
-// task_descr.className = "task-descr";
-task_status.className = "task-status";
-task_time.className = "task-date";
-task_priority.className = "task-priority";
+let task_status = document.getElementById("status");
 
-task_title.textContent = "task_title";
+let task_time = document.getElementById("time");
+let card_time = document.createElement('div');
+card_time.className = "task-time";
 
-let submit = document.querySelector(".submit");
+let task_date = document.getElementById("date");
+let card_date = document.createElement('div');
+card_time.className = "task-date";
 
-submit.onclick = function() {
-    let card = document.createElement("div");
-    card.classname = "task";
+let task_priority = document.getElementById("priority");
 
-    card.appendChild(task_title);
-    card.appendChild(document.createTextNode (task_time));
-    card.appendChild(document.createTextNode( task_date));
+let Add_card = document.getElementById("submit");
+
+Add_card.onclick = function() {
+    let card = document.createElement('div');
+    card.className = "task";
+
+    card_title.appendChild(document.createTextNode(task_title.value));
+    card_time.appendChild(document.createTextNode(task_time.value));
+    card_date.appendChild(document.createTextNode( task_date.value));
+    // card_descr.appendChild(document.createTextNode(task_descr.value));
 
     let task_delete = document.createElement('button');
-    task_delete.onclick = function(){
+    task_delete.textContent = "Delete";
+    task_delete.className = "task-delete";
+    let delete_Id = new Date();
+    task_delete.Id = "delete_Id";
+    console.log(task_delete.Id);
+    delete_Id.onclick = function(){
         card.remove();
     }
 
     let task_done = document.createElement('dive');
-    task_done.textContent = "Done";
+    let done_label = document.createElement('label')
+    done_label.textContent = "Done";
+    task_done.className = "task-done"
+    done_label.htmlFor = "done";
+    task_done.appendChild(done_label);
     let done_input = document.createElement('input');
+    done_input.id = "done";
     done_input.type = 'checkbox' ;
     task_done.appendChild(done_input);
 
+    card.appendChild(card_title);
+    card.appendChild(card_time);
+    card.appendChild(card_date);
+    card.appendChild(card_descr);
     card.appendChild(task_delete);
     card.appendChild(task_done);
-    
-    if (task_status === "To Do") {
-        document.getElementsByClassName(".To-Do").appendChild(card);
+
+    switch (task_priority.value) {
+        case "P1":
+            card.style.border = "2px solid red";
+            card.style.borderLeft = "10px solid red";
+            break;
+        
+        case "P2":
+            card.style.border = "2px solid green";
+            card.style.borderLeft = "10px solid green";
+            break;
+        case "P3":
+            card.style.border = "2px solid blue";
+            card.style.borderLeft = "10px solid blue";
+            break;
+        
+        default:
+            break;
     }
-    else if (task_status === "Doing") {
-        document.getElementsByClassName(".Doing").appendChild(card);
+
+    if (task_status.value === "To Do") {
+        document.getElementById("To-Do").appendChild(card);
     }
-    else {
-        // document.getElementsByClassName(".Done").appendChild(card);
+    else if (task_status.value === "Doing") {
+        document.getElementById("Doing").appendChild(card);
+    }
+    else if (task_status.value === "Done") {
+        document.getElementById("Done").appendChild(card);
     }
 
     task_title.value = "";
-    // task_descr.value = "";
+    task_descr.value = "";
     task_time.value = "";
     task_date.value = "";
 
