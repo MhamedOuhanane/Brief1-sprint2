096d78fe047bf1a86fb9bd0fe38ed286b815956d
MhamedOuhanane <mhmdeouhnan60@gmail.com>
2024-11-03 01:08:56 +0100

    modifié le style du ste web et changé le script js de button de modification

--- js/doss.js
+++ js/doss.js
@@ -6,7 +6,9 @@ let add_task = document.getElementById("add-task");
 let delete_task = document.getElementById("delete-task");
 let cancel_task = document.getElementById("cancel-task");
 let Ajout_card = document.getElementById("Ajout-card");
-let List_card = document.getElementById("List-card");
+let modal_Shadow = document.getElementById("Shadow");
+
+let task_modifier = document.createElement('button');
 
     //  Masquer & Afficher
 function AddTransform(disp1, disp2 , vis_hid) {
@@ -19,9 +21,9 @@ function AddTransform(disp1, disp2 , vis_hid) {
 
     Ajout_card.classList.remove(disp1);
     Ajout_card.classList.add(disp2);
-
-    List_card.classList.add(disp1);
-    List_card.classList.remove(disp2);
+    
+    modal_Shadow.classList.remove(disp1);
+    modal_Shadow.classList.add(disp2);
 }
 
 
@@ -58,14 +60,10 @@ add_task.onclick = function() {
         let card = document.createElement('div');
         card.className = "task";
 
-        function RemplirCard(CARD){
-            card_title.innerHTML = task_title.value ;
-            card_time.innerHTML = task_time.value;
-            card_date.innerHTML = task_date.value ;
-            card_descr.innerHTML = task_descr.value;
-        }
-        
-        RemplirCard(card);
+        card_title.innerHTML = task_title.value ;
+        card_time.innerHTML = task_time.value;
+        card_date.innerHTML = task_date.value ;
+        card_descr.innerHTML = task_descr.value;
 
         let task_delete = document.createElement('button');
         task_delete.textContent = "Delete";
@@ -146,40 +144,94 @@ add_task.onclick = function() {
         ChoisStatus(card);
         ChoisPriority(card , 1);
         
-        let task_modifier = document.createElement('button');
+        
         task_modifier.className = "task-modifier";
         task_modifier.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#90caf9" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152L0 424c0 48.6 39.4 88 88 88l272 0c48.6 0 88-39.4 88-88l0-112c0-13.3-10.7-24-24-24s-24 10.7-24 24l0 112c0 22.1-17.9 40-40 40L88 464c-22.1 0-40-17.9-40-40l0-272c0-22.1 17.9-40 40-40l112 0c13.3 0 24-10.7 24-24s-10.7-24-24-24L88 64z"/></svg>'
         card.appendChild(task_modifier);
 
-        task_modifier.onclick = function() {
-            AddTransform("d-none", "d-block" , "hidden");
+    //     task_modifier.onclick = function() {
+    //         AddTransform("d-none", "d-block" , "hidden");
 
-            cancel_task.onclick = function(){
-                AddTransform("d-block", "d-none" , "visible");
-            }
-            Add_card.classList.add("d-none");
+    //         cancel_task.onclick = function(){
+    //             AddTransform("d-block", "d-none" , "visible");
+    //         }
+    //         Add_card.classList.add("d-none");
             
-            let modifier = document.getElementById("Modifier");
-            modifier.classList.remove("d-none");
-            modifier.onclick = function(){
+    //         task_title.value = document.getElementById("title").value;
+    //         task_descr.value = document.getElementById("description").value;
+    //         task_status.value = document.getElementById("status").value;
+    //         task_time.value = document.getElementById("time").value;
+    //         task_date.value = document.getElementById("date").value;
+    //         task_priority.value = document.getElementById("priority").value;
+
+
+    //         let modifier = document.getElementById("Modifier");
+    //         modifier.classList.remove("d-none");
+    //         modifier.onclick = function(){
                 
-                RemplirCard(card);
-                ChoisStatus(card);
-                ChoisPriority(card , 1);
+    //             card_title.innerHTML = task_title.value ;
+    //             card_time.innerHTML = task_time.value;
+    //             card_date.innerHTML = task_date.value ;
+    //             card_descr.innerHTML = task_descr.value;
 
-                AddTransform("d-block", "d-none" , "visible");
-                modifier.classList.add("d-none");
-                Add_card.classList.remove("d-none");
+    //             ChoisStatus(card);
+    //             ChoisPriority(card , 1);
 
-            }
-            cancel_task.onclick = function(){
-                AddTransform("d-block", "d-none" , "visible");
-                modifier.classList.add("d-none");
-                Add_card.classList.remove("d-none");
-            }
-        }
+    //             AddTransform("d-block", "d-none" , "visible");
+    //             modifier.classList.add("d-none");
+    //             Add_card.classList.remove("d-none");
+
+    //         }
+    //         cancel_task.onclick = function(){
+    //             AddTransform("d-block", "d-none" , "visible");
+    //             modifier.classList.add("d-none");
+    //             Add_card.classList.remove("d-none");
+    //         }
+    //     }
 
         AddTransform("d-block", "d-none" , "visible");
+    }
+    
+    
+
+    task_modifier.onclick = function() {
+        AddTransform("d-none", "d-block" , "hidden");
+
+        cancel_task.onclick = function(){
+            AddTransform("d-block", "d-none" , "visible");
+        }
+        Add_card.classList.add("d-none");
+        
+        task_title.value = document.getElementById("title").value;
+        task_descr.value = document.getElementById("description").value;
+        task_status.value = document.getElementById("status").value;
+        task_time.value = document.getElementById("time").value;
+        task_date.value = document.getElementById("date").value;
+        task_priority.value = document.getElementById("priority").value;
+
+
+        let modifier = document.getElementById("Modifier");
+        modifier.classList.remove("d-none");
+        modifier.onclick = function(){
+            
+            card_title.innerHTML = task_title.value ;
+            card_time.innerHTML = task_time.value;
+            card_date.innerHTML = task_date.value ;
+            card_descr.innerHTML = task_descr.value;
+
+            ChoisStatus(card);
+            ChoisPriority(card , 1);
+
+            AddTransform("d-block", "d-none" , "visible");
+            modifier.classList.add("d-none");
+            Add_card.classList.remove("d-none");
+
+        }
+        cancel_task.onclick = function(){
+            AddTransform("d-block", "d-none" , "visible");
+            modifier.classList.add("d-none");
+            Add_card.classList.remove("d-none");
+        }
     }
 
     cancel_task.onclick = function(){
